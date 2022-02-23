504a35b598403bacddb4338505a10f6777aec960
WuWenbin <541330190@qq.com>
2022-02-23 12:38:23 +0800

    fix: remove foo deps and add version in bar

--- packages/lab/pages/index.js
+++ packages/lab/pages/index.js
@@ -1,11 +1,9 @@
-import foo from 'foo'
 import Bar from 'bar'
 
 export default function Home() {
   return (
     <div>
       Imported modules from another workspace:
-      <pre>{foo}</pre>
       <Bar />
     </div>
   )
